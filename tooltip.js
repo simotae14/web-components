08478c26281462b854bd22b3e0d19654715202bd
Simone Taeggi <startae14@gmail.com>
2026-06-20 19:02:18 +0200

    009 Defining the Template in JavaScript

--- tooltip.js
+++ tooltip.js
@@ -5,8 +5,10 @@ class Tooltip extends HTMLElement {
     // default text
     this._tooltipText = 'Some dummy tooltip text.';
     this.attachShadow({ mode: 'open' });
-    const template = document.querySelector('#tooltip-template');
-    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.shadowRoot.innerHTML = `
+      <slot>Some default</slot>
+      <span> (?)</span>
+    `;
   }
 
   connectedCallback() {
