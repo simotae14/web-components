e91527fe48d61286477f032cdd27c3e066c5f589
Simone Taeggi <startae14@gmail.com>
2026-06-20 19:22:53 +0200

    011 Extending Built-In Elements

---
+++ confirm-link.js
@@ -0,0 +1,13 @@
+class ConfirmLink extends HTMLAnchorElement {
+  connectedCallback() {
+    this.addEventListener('click', event => {
+      if (!confirm('Do you really want to leave?')) {
+        event.preventDefault();
+      }
+    });
+  }
+}
+
+customElements.define('tae-confirm-link', ConfirmLink, {
+  extends: 'a'
+});
