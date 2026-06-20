b68da7a75deb5fb526a751b10390a249c095288b
Simone Taeggi <startae14@gmail.com>
2026-06-20 19:09:06 +0200

    010 using Style Tags in the Shadow DOM

--- tooltip.js
+++ tooltip.js
@@ -6,6 +6,14 @@ class Tooltip extends HTMLElement {
     this._tooltipText = 'Some dummy tooltip text.';
     this.attachShadow({ mode: 'open' });
     this.shadowRoot.innerHTML = `
+      <style>
+        div {
+          background-color: black;
+          color: white;
+          position: absolute;
+          z-index: 10;
+        }
+      </style>
       <slot>Some default</slot>
       <span> (?)</span>
     `;
@@ -22,16 +30,11 @@ class Tooltip extends HTMLElement {
     this.shadowRoot.appendChild(tooltipIcon);
     // set custom element position
     this.style.position = 'relative';
-
   }
 
   _showTooltip() {
     this._tooltipContainer = document.createElement('div');
     this._tooltipContainer.textContent = this._tooltipText;
-    this._tooltipContainer.style.backgroundColor = 'black';
-    this._tooltipContainer.style.color = 'white';
-    this._tooltipContainer.style.position = 'absolute';
-    this._tooltipContainer.style.zIndex = 10;
     this.shadowRoot.appendChild(this._tooltipContainer);
   }
 
